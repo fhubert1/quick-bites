84c7df3f07b3269fb60904ee22869785109f73e4
saba pervez <sabah.junaid@outlook.com>
2024-07-27 01:25:06 -0400

    added contact,signup & login forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "../src/App.module.css";
-import { Contact } from "./components/ContactUs/ContactUs";
+import { Contact } from './components/ContactUs/ContactUs';
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
---
+++ client/src/components/LoginFrom/LoginForm.jsx
@@ -0,0 +1,33 @@
+// src/components/Login.js
+import React from 'react';
+
+const Login = ({ formData, handleChange, errors }) => (
+  <>
+    <div className="formGroup">
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        className={`formInput ${errors.email ? 'inputError' : ''}`}
+      />
+      {errors.email && <span className="error">{errors.email}</span>}
+    </div>
+    <div className="formGroup">
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        className={`formInput ${errors.password ? 'inputError' : ''}`}
+      />
+      {errors.password && <span className="error">{errors.password}</span>}
+    </div>
+  </>
+);
+
+export default Login;
