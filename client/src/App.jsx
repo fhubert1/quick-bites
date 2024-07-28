ecbda32795079db1a4be447412e44f1a1d078a39
scerda8 <shandycerda@shandys-mbp-2.mynetworksettings.com>
2024-07-27 23:26:38 -0400

    changes and deltions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import styles from "../src/App.module.css";
-import { Contact } from "./components/ContactUs/ContactUs";
 import { Navbar } from "./components/Navbar/Navbar";
+
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Contact />
+    
+
+
+
     </div>
   );
 }
