9cdd1fcbbfadc094b3c7dd90be2ee4ddbadd562c
saba pervez <sabah.junaid@outlook.com>
2024-07-31 22:44:24 -0400

    fixed the Gif and forms styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import '../assets/styles/Home.css'; 
-
+import '../assets/styles/Home.css';
+import quickbitesGif from '../assets/icon/quickbitesGif.gif';
 
 const Home = () => (
   <div className="homepage">
+    <img src={quickbitesGif} alt="Central GIF" className="centeredGif" />
   </div>
 );
 
 export default Home;
+
+
