c9be26cdeec9b86740c0c7b6aa7a9d00b28802d1
Sam <sambolton.kw@gmail.com>
2024-08-01 18:47:12 -0400

    conditional render

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,44 @@
 // import React from "react";
 import styles from './Navbar.module.css';
 import {Link} from 'react-router-dom';
+import Auth from "../../../utils/auth";
 
  export const Navbar = () => {
-
+    if (Auth.loggedIn()) {
+      return (
+        <div className={styles.navbar}>
+        <nav>
+          <ul className={styles.nav}>
+            <li>
+                <Link to='/' >
+                Home
+                </Link>
+            </li>
+            <li>
+               <Link to="/menu" className={styles.navlink}>
+                Menu
+                </Link>
+            </li>
+        <li>
+           <Link to="/contact">
+            Contact Us
+            </Link>
+        </li>
+        <li>
+            <Link to="/OrderHistory">
+            Order History!
+           </Link>
+        </li>
+        <li>
+        <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+        </li>
+          </ul>
+        </nav>
+      </div>
+      );
+    } else {
     return (
         <div className={styles.navbar}>
         <nav>
@@ -38,6 +73,7 @@ import {Link} from 'react-router-dom';
       </div>
 
   );
+};
 };
 
 export default Navbar;
