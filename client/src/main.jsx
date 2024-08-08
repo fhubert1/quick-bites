e77a79a0c1736ccd425983179a537534c16a1143
saba pervez <sabah.junaid@outlook.com>
2024-08-08 00:06:45 -0400

    fixed the bugs in sign in and updated readme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,39 +1,63 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import "@fontsource/bungee-shade";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import  Menu  from "./pages/Menu";
+import { ApolloProvider, InMemoryCache, ApolloClient, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Menu from "./pages/Menu";
 import Contact from "./pages/Contact";
-//import Signup from "./pages/Signup";
-//import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound.jsx";
 import RestaurantDetail from "./pages/RestaurantDetail.jsx";
 import Cart from './components/Cart/Cart.jsx';
 
+// Apollo Client setup
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
+// Router setup
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>,
-    errorElement: <NotFound/>, 
+    element: <App />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
-        element: <Home/>
-      }, {
+        element: <Home />
+      },
+      {
         path: '/menu',
-        element: <Menu/>
-      }, {
+        element: <Menu />
+      },
+      {
         path: '/contact',
-        element: <Contact/>
-      }, 
+        element: <Contact />
+      },
       {
         path: '/restaurant/:id',
         element: <RestaurantDetail />
-      }, {
+      },
+      {
         path: '/cart',
         element: <Cart />
       }
@@ -41,6 +65,9 @@ const router = createBrowserRouter([
   },
 ]);
 
+// Render the application with ApolloProvider
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
