f6f52bfaa7c5c88728184b5510e30260b6f18e00
scerda8 <shandycerda@shandys-mbp-2.mynetworksettings.com>
2024-08-12 00:05:10 -0400

    testing again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/quickbites");
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://cerdashandy:bJhcLNfBKOaLE3rw@cluster0.smk5zid.mongodb.net/quickbites_db");
 
 module.exports = mongoose.connection;
