dc660e811d824397c8e338acfd6731d7a92dc991
scerda8 <shandycerda@shandys-mbp-2.mynetworksettings.com>
2024-08-12 00:44:32 -0400

    extra }

--- server/server.js
+++ server/server.js
@@ -58,7 +58,6 @@ const startApolloServer = async () => {
             console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
         });
     });
-};
 
 // Call the async function to start the server
 startApolloServer();
